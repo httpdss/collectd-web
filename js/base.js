c6dce2d8f74318ac7bad77fd1e072484130d4551
Kenny Belitzky <kennybz@gmail.com>
2009-04-12 17:40:47 -0300

    css and js separation

---
+++ js/base.js
@@ -0,0 +1,45 @@
+
+var remove_button = '<div class="icons ui-state-default ui-corner-all"><span class="ui-icon ui-icon-close"/></div>'
+
+$(document).ready(function() {  
+	$.AjaxifyDefaults.loading_target = '#loader';
+
+	$('#hosts a').livequery( function(){
+        $(this).ajaxify({
+            target:'#plugins',
+        });
+    });
+
+	$('#plugins a').livequery( function(){
+		$(this).ajaxify({
+			target:'#graph-container',
+			animateOut:{opacity:'0'},
+			animateOutSpeed:300,
+			animateIn:{opacity:'1'},
+			animateInSpeed:300,
+		});
+	});
+	$('#hosts').load('/cgi-bin/collection.modified.cgi');
+
+    $('li.graph-image').livequery(function(){
+            $(this).each(function(){
+                $(this).prepend(remove_button);
+                $('li.graph-image .ui-icon-close').click(function (){ 
+                        $(this).parent().parent().remove();
+                    });
+                });
+            });
+    $('.icons').livequery(function(){
+            $(this).each(function(){
+                $(this).hover(
+                    function() { $(this).addClass('ui-state-hover'); },
+                    function() { $(this).removeClass('ui-state-hover'); }
+                    );
+                });
+            });
+    $('.sortable').livequery(function(){
+            $(this).each(function(){
+                    $(this).sortable();
+                });
+            });
+}); 
