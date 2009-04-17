ce8beb1aeebd695dc026882fb8497ca151bd857c
Kenny Belitzky <kennybz@gmail.com>
2009-04-17 01:34:37 -0300

    added host filtering and selectable
    added js code to be able to filter hostnames. Also did js code to
    be able to add a "selected" class to list items on click from hosts
    and plugins. still needs css style for li.selected

--- js/base.js
+++ js/base.js
@@ -1,45 +1,58 @@
-
 var remove_button = '<div class="icons ui-state-default ui-corner-all"><span class="ui-icon ui-icon-close"/></div>'
 
 $(document).ready(function() {  
-	$.AjaxifyDefaults.loading_target = '#loader';
+        $.AjaxifyDefaults.loading_target = '#loader';
 
-	$('#hosts a').livequery( function(){
-        $(this).ajaxify({
-            target:'#plugins',
-        });
-    });
+        $('#hosts a').livequery( function(){
+            $(this).ajaxify({
+                    target:'#plugins',
+                });
+            });
 
-	$('#plugins a').livequery( function(){
-		$(this).ajaxify({
-			target:'#graph-container',
-			animateOut:{opacity:'0'},
-			animateOutSpeed:300,
-			animateIn:{opacity:'1'},
-			animateInSpeed:300,
-		});
-	});
-	$('#hosts').load('/cgi-bin/collection.modified.cgi');
+        $('#plugins a').livequery( function(){
+            $(this).ajaxify({
+                    target:'#graph-container',
+                    animateOut:{opacity:'0'},
+                    animateOutSpeed:300,
+                    animateIn:{opacity:'1'},
+                    animateInSpeed:300,
+                });
+            });
+        $('#hosts').load('/cgi-bin/collection.modified.cgi');
 
-    $('li.graph-image').livequery(function(){
+        $('li.graph-image').livequery(function(){
             $(this).each(function(){
-                $(this).prepend(remove_button);
-                $('li.graph-image .ui-icon-close').click(function (){ 
+                    $(this).prepend(remove_button);
+                    $('li.graph-image .ui-icon-close').click(function (){ 
                         $(this).parent().parent().remove();
                     });
                 });
             });
-    $('.icons').livequery(function(){
+
+        $('.icons').livequery(function(){
             $(this).each(function(){
                 $(this).hover(
-                    function() { $(this).addClass('ui-state-hover'); },
-                    function() { $(this).removeClass('ui-state-hover'); }
-                    );
-                });
+                        function() { $(this).addClass('ui-state-hover'); },
+                        function() { $(this).removeClass('ui-state-hover'); }
+                );
             });
-    $('.sortable').livequery(function(){
+        });
+
+        $('.sortable').livequery(function(){
             $(this).each(function(){
-                    $(this).sortable();
-                });
+                $(this).sortable();
             });
+        });
+
+        $("#host-filter").keyup(function () {
+            var searchText = $(this).val();
+            $("#hosts li").hide();
+            $("#hosts li:contains("+searchText+")").show();
+        });
+
+        $('#hosts li, #plugins li').livequery(function(){
+            $(this).click(function (){
+                    $(this).addClass("selected");
+            });
+        });
 }); 
