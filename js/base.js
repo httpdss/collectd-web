9ef98154852caec9b8141cbc301bdeb87c1050f6
Kenneth Belitzky <kenny@belitzky.com>
2010-03-27 20:30:49 -0300

    update and new features

--- js/base.js
+++ js/base.js
@@ -1,63 +1,112 @@
-var remove_button = '<div class="icons ui-state-default ui-corner-all"><span class="ui-icon ui-icon-close"/></div>'
+//var remove_button = '<div class="icons ui-state-default ui-corner-all"><span class="ui-icon ui-icon-close"/></div>'
+var remove_button = '<span class="ui-icon ui-icon-close"/>';
+/**
+ * Get the id of the container for the selected element
+ * 
+ * @param {Object}
+ *            elem
+ */
+function get_container(elem) {
+	return $(elem).parent().parent().parent().attr('id');
+}
 
-$(document).ready(function() {  
-        $.AjaxifyDefaults.loading_target = '#loader';
+/**
+ * default control container
+ * 
+ * @param {Object}
+ *            txt
+ */
+function control_container(txt) {
+	return '<div class="icons ui-state-default ui-corner-all">' + txt + '</div>';
+}
 
-        $('#hosts a').livequery( function(){
-            $(this).ajaxify({
-                    target:'#plugins',
-                });
-            });
+/**
+ * check which container anchor is clicked and load the apropiate container
+ * 
+ * @param {Object}
+ *            url
+ */
+var load_url = function() {
+	var url = $(this).attr('href');
+	var place = get_container(this) == 'hosts-container' ? '#plugins'
+			: '#graph-container';
+	$(place).load(url);
+	return false;
+}
 
-        $('#plugins a').livequery( function(){
-            $(this).ajaxify({
-                    target:'#graph-container',
-                    animateOut:{opacity:'0'},
-                    animateOutSpeed:300,
-                    animateIn:{opacity:'1'},
-                    animateInSpeed:300,
-                });
-            });
-        $('#hosts').load('/cgi-bin/collection.modified.cgi');
+$(document).ready(function() {
+	
+	$("#loading").ajaxStart(function() {
+		$(this).show();
+	});
+	
+	$("#loading").ajaxStop(function() {
+		$(this).hide();
+		$('.sortable').sortable();
+	});
+	
+	$('#hosts').load('/cgi-bin/collection.modified.cgi');
+	
+	$('#menu-tabs').tabs();
 
-        $('li.graph-image').livequery(function(){
-            $(this).each(function(){
-                    $(this).prepend(remove_button);
-                    $('li.graph-image .ui-icon-close').click(function (){ 
-                        $(this).parent().parent().remove();
-                    });
-                });
-            });
+	$(".date-field").datepicker();
 
-        $('.icons').livequery(function(){
-            $(this).each(function(){
-                $(this).hover(
-                        function() { $(this).addClass('ui-state-hover'); },
-                        function() { $(this).removeClass('ui-state-hover'); }
-                );
-            });
-        });
-
-        $('.sortable').livequery(function(){
-            $(this).each(function(){
-                $(this).sortable();
-            });
-        });
-
-
-        $('#hosts li, #plugins li').livequery(function(){
-            $(this).click(function (){
-                    $(this).addClass("selected");
-            });
-        });
-
-        $("#host-filter").livequery(function(){
-            $(this).keyup(function () {
-                var searchText = $(this).val();
-                $("#hosts li").hide();
-                $("#hosts li:contains("+searchText+")").show();
-            });
-            $(this).focus();
-        });
-
-}); 
+	$("#clock").jclock();
+	
+	$("#clock-server").jclock();
+	$("#clock-server-slider").slider();
+	
+	$('button').button();
+	
+	$('#hosts a, #plugins a').live('click', load_url);
+	
+	$('li.graph-image .ui-icon-close').live('click', function() {
+		$(this).parent().parent().remove();
+	});
+	
+	$("#slide-menu-container .ui-widget-header").click(function() {
+		$("#slide-menu-container .ui-widget-content").slideToggle("slow");
+		$(this).toggleClass("active");
+		return false;
+	});
+	
+	$('.icons, .fg-button').livequery(function() {
+		$(this).each(function() {
+			$(this).hover(function() {
+				$(this).addClass('ui-state-hover');
+			}, function() {
+				$(this).removeClass('ui-state-hover');
+			});
+		});
+	});
+	
+	$("#host-filter").live('keyup', function() {
+		var searchText = $(this).attr('text');
+		$("#hosts li").hide();
+		if (searchText == "") {
+			$("#hosts li").show();
+		} else {
+			$("#hosts li:contains(" + searchText + ")").show();
+		}
+	
+		$(this).focus();
+	
+	});
+	
+	$('#hosts a, #plugins a').live('click', function() {
+		$(this).addClass("selected");
+	});
+	
+	$("#timespan-menu li").live(
+			'click',
+			function() {
+				$("#timespan-menu li").each(function() {
+					$(this).removeClass("selected");
+				});
+				var timespan = $(this).html();
+				$("li.graph-image li").hide();
+				$("li.graph-image li." + timespan).show();
+				$("#timespan-menu li:contains(" + timespan + ")").addClass(
+						"selected");
+		});
+});
